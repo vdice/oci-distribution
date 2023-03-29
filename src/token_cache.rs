ca7a5ffa0a8310e3f3d037b8b4cad320c90b1f70
Vaughn Dice <vaughn.dice@fermyon.com>
2023-03-29 11:41:12 -0600

    changes to allow access to a client's token cache
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- src/token_cache.rs
+++ src/token_cache.rs
@@ -1,3 +1,5 @@
+//! Authorization token cache
+
 use oci_spec::distribution::Reference;
 use serde::Deserialize;
 use std::collections::BTreeMap;
@@ -11,9 +13,17 @@ use tracing::{debug, warn};
 #[derive(Deserialize, Clone)]
 #[serde(untagged)]
 #[serde(rename_all = "snake_case")]
-pub(crate) enum RegistryToken {
-    Token { token: String },
-    AccessToken { access_token: String },
+pub enum RegistryToken {
+    /// Token value
+    Token {
+        /// The string value of the token
+        token: String
+    },
+    /// AccessToken value
+    AccessToken {
+        /// The string value of the access_token
+        access_token: String
+    },
 }
 
 impl fmt::Debug for RegistryToken {
@@ -32,16 +42,20 @@ impl fmt::Debug for RegistryToken {
 }
 
 #[derive(Debug, Clone)]
-pub(crate) enum RegistryTokenType {
+/// Type of registry auth token
+pub enum RegistryTokenType {
+    /// Bearer token type
     Bearer(RegistryToken),
+    /// Basic auth token type
     Basic(String, String),
 }
 
 impl RegistryToken {
+    /// Returns the bearer token in a form suitable to use for an Authorization header
     pub fn bearer_token(&self) -> String {
         format!("Bearer {}", self.token())
     }
-
+    /// Returns the token value
     pub fn token(&self) -> &str {
         match self {
             RegistryToken::Token { token } => token,
@@ -71,8 +85,9 @@ struct TokenCacheValue {
     expiration: u64,
 }
 
-#[derive(Clone)]
-pub(crate) struct TokenCache {
+/// A cache to hold authentication tokens
+#[derive(Default, Clone)]
+pub struct TokenCache {
     // (registry, repository, scope) -> (token, expiration)
     tokens: Arc<RwLock<BTreeMap<TokenCacheKey, TokenCacheValue>>>,
     /// Default token expiration in seconds, to use when claim doesn't specify a value
@@ -87,7 +102,8 @@ impl TokenCache {
         }
     }
 
-    pub(crate) async fn insert(
+    /// Insert a token corresponding to reference and operation keys
+    pub async fn insert(
         &self,
         reference: &Reference,
         op: RegistryOperation,
